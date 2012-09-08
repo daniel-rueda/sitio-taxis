0a4e4bbef635e739fdb2b302d8f2b4759c5e2ced
Daniel Rueda Jimenez <rujida@gmail.com>
2012-09-08 01:40:56 -0500

    Agregado RestKit

--- SitioTaxis/Data model/DRDataModel.h
+++ SitioTaxis/Data model/DRDataModel.h
@@ -8,7 +8,8 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
-#import <CoreData/CoreData.h>
+#import <RestKit/RestKit.h>
+#import <RestKit/CoreData.h>
 
 @interface DRDataModel : NSObject
 
