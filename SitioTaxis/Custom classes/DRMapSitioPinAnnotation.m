94650475be5297c6b5ce5420f9133f1924f088ae
Cristhian Velazco <cristhianvs@yahoo.com>
2012-09-08 08:16:35 -0500

    Mapa Creado
    MapPinAnnotation

---
+++ SitioTaxis/Custom classes/DRMapSitioPinAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  DRMapSitioPinAnnotation.m
+//  SitioTaxis
+//
+//  Created by Cristhian Velazco Solano on 08/09/12.
+//  Copyright (c) 2012 Planet Media. All rights reserved.
+//
+
+#import "DRMapSitioPinAnnotation.h"
+
+@implementation DRMapSitioPinAnnotation
+
+@end
