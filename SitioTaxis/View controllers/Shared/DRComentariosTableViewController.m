0afdaf34c93316498a80de4c8a3995aee2d09c81
Daniel Rueda Jimenez <rujida@gmail.com>
2012-09-08 19:08:50 -0500

    Agregada clase para comentarios

---
+++ SitioTaxis/View controllers/Shared/DRComentariosTableViewController.m
@@ -0,0 +1,68 @@
+//
+//  DRComentariosTableViewController.m
+//  SitioTaxis
+//
+//  Created by Daniel Rueda Jimenez on 9/8/12.
+//  Copyright (c) 2012 Planet Media. All rights reserved.
+//
+
+#import "DRComentariosTableViewController.h"
+
+@interface DRComentariosTableViewController ()
+{
+    NSArray *_comentarios;
+}
+@end
+
+@implementation DRComentariosTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [_comentarios count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    cell.textLabel.text = @"Hola mundo";
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
